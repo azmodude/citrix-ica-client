52dd2fe57895ad0207edc288cd4e6104c77afd4a
Gordon Schulz <gordon@gordonschulz.de>
2022-04-06 08:10:03 +0200

    Properly quote substitutions

--- citrix-run.sh
+++ citrix-run.sh
@@ -8,7 +8,7 @@ else
   icafile_container=$(basename "${1}")
 fi
 
-xhost +SI:localuser:$(id -u -n)
+xhost +SI:localuser:"$(id -u -n)"
 
 podman run --rm --detach \
   --user citrix \
@@ -19,10 +19,10 @@ podman run --rm --detach \
   -e DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /dev/dri:/dev/dri \
-  -v /run/user/$(id -u)/pulse:/run/user/1000/pulse \
+  -v "/run/user/$(id -u)/pulse":/run/user/1000/pulse \
   -e PULSE_SERVER=/run/user/1000/pulse/native \
   -v "${icafile_host}:/ica/${icafile_container}":Z \
   --security-opt label=type:container_runtime_t \
   citrix-ica-client "/ica/${icafile_container}"
 
-xhost -SI:localuser:$(id -u -n)
+xhost -SI:localuser:"$(id -u -n)"
